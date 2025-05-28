3a845ec145e65619e71a785614f2008759e2f3e3
dedestem <david@davidnet.net>
2025-05-28 18:53:51 +0200

    Fix add index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './components/Button.svelte';
+export { default as CenterX } from './components/CenterX.svelte';
 export { default as Gap } from './components/Gap.svelte';
 export { default as IconButton } from './components/IconButton.svelte';
 export { default as LinkButton } from './components/LinkButton.svelte';
