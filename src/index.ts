b8c3a087acde70d70e73cc5b1513a13f6e13fe04
dedestem <david@davidnet.net>
2025-05-28 16:26:23 +0200

    FIIIXIXX

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as Button } from './components/Button.svelte';
 export { default as Gap } from './components/Gap.svelte';
 export { default as IconButton } from './components/IconButton.svelte';
 export { default as LinkButton } from './components/LinkButton.svelte';
+export { default as LinkIconButton } from './components/LinkIconButton.svelte';
 export { default as Modal } from './components/Modal.svelte';
