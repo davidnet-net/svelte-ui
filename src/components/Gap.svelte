e000088451f251e1e94c8701e9538df9a6b07130
dedestem <david@davidnet.net>
2025-05-28 15:16:11 +0200

    Gap element

---
+++ src/components/Gap.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let height: string = "5px";
+  export let width: string = "5px";
+</script>
+
+<div style="height: {height}; width: {width};"></div>
+
+<style>
+  div {
+    background-color: transparent;
+    color: transparent;
+    margin: 0px;
+    padding: 0px;
+  }
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './components/Button.svelte';
+export { default as Gap } from './components/Gap.svelte';
 export { default as IconButton } from './components/IconButton.svelte';
 export { default as LinkButton } from './components/LinkButton.svelte';
 export { default as Modal } from './components/Modal.svelte';
