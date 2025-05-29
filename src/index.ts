a9d6170f9e833142935c64d1dbd5ff4abe2f6a49
dedestem <david@davidnet.net>
2025-05-29 12:37:33 +0200

    Add imagecard to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as Button } from './components/Button.svelte';
 export { default as CenterX } from './components/CenterX.svelte';
 export { default as Gap } from './components/Gap.svelte';
 export { default as IconButton } from './components/IconButton.svelte';
+export { default as ImageCard } from './components/ImageCard.svelte';
 export { default as LinkButton } from './components/LinkButton.svelte';
 export { default as LinkIconButton } from './components/LinkIconButton.svelte';
 export { default as Modal } from './components/Modal.svelte';
