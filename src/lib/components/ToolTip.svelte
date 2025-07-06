40acf28f6cde43ee7c1b93caa96edb44dab148ee
dedestem <david@davidnet.net>
2025-07-06 18:53:50 +0200

    Make that arrow move on the tooltip

--- src/lib/components/ToolTip.svelte
+++ src/lib/components/ToolTip.svelte
@@ -1,17 +1,51 @@
 <script lang="ts">
-    export let text: string;
+  import { onMount } from 'svelte';
+  import { tick } from 'svelte';
+
+  export let text: string;
+  let tooltipEl: HTMLDivElement;
+  let nudgeX = 0;
+
+  async function adjustPositionOnce() {
+    await tick();
+
+    if (!tooltipEl) return;
+
+    const rect = tooltipEl.getBoundingClientRect();
+    const viewportWidth = window.innerWidth;
+
+    if (rect.left < 0) {
+      nudgeX = -rect.left + 8;
+    } else if (rect.right > viewportWidth) {
+      nudgeX = viewportWidth - rect.right - 8;
+    } else {
+      nudgeX = 0;
+    }
+  }
+
+  onMount(() => {
+    adjustPositionOnce();
+  });
+
+  $: if (text) {
+    adjustPositionOnce();
+  }
 </script>
 
-<div class="tooltip" role="tooltip">
-    {text}
+<div
+  class="tooltip"
+  role="tooltip"
+  bind:this={tooltipEl}
+  style="transform: translateX(calc(-50% + {nudgeX}px)); --arrow-offset: {nudgeX}px;"
+>
+  {text}
 </div>
 
 <style>
-.tooltip {
+  .tooltip {
     position: absolute;
     top: 100%;
     left: 50%;
-    transform: translateX(-50%);
     margin-top: 10px;
     background-color: var(--token-color-surface-inverse-raised-normal);
     color: var(--token-color-text-inverse-normal);
@@ -24,18 +58,16 @@
     z-index: 10;
     box-sizing: border-box;
     line-height: 1;
-}
+  }
 
-/* Arrow with tip facing UP */
-.tooltip::after {
+  .tooltip::after {
     content: "";
     position: absolute;
-    bottom: 100%; /* position arrow just above tooltip */
+    bottom: 100%;
     left: 50%;
-    transform: translateX(-50%);
-    border-width: 0 6px 6px 6px; /* height 6px pointing up */
+    transform: translateX(calc(-50% - var(--arrow-offset, 0px)));
+    border-width: 0 6px 6px 6px;
     border-style: solid;
     border-color: transparent transparent var(--token-color-surface-inverse-raised-normal) transparent;
-}
-
+  }
 </style>
