1a8c5189c0b8e645ba8cdba19f30d2e3c5a28c56
dedestem <david@davidnet.net>
2025-10-29 15:10:23 +0100

    Add wrapping to the FlexWrapper

--- src/lib/components/Layout/FlexWrapper.svelte
+++ src/lib/components/Layout/FlexWrapper.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
 	export let direction: "column" | "row" | "column-reverse" | "row-reverse" = "column";
-	export let gap: string | undefined = "0px;";
-	export let height: string | undefined = "fit-content";
-	export let width: string | undefined = "fit-content";
+	export let gap: string = "0px";
+	export let height: string = "fit-content";
+	export let width: string = "fit-content";
 	export let justifycontent: string = "center";
 	export let alignitems: string = "center";
+	export let overflow: "visible" | "hidden" | "scroll" | "auto" = "visible";
+	export let wrap: "nowrap" | "wrap" | "wrap-reverse" = "nowrap";
 </script>
 
-<div style="flex-direction: {direction}; gap: {gap}; height: {height}; width: {width}; justify-content: {justifycontent}; align-items: {alignitems};">
+<div
+	style="
+		display: flex;
+		flex-direction: {direction};
+		gap: {gap};
+		height: {height};
+		width: {width};
+		justify-content: {justifycontent};
+		align-items: {alignitems};
+		overflow: {overflow};
+		flex-wrap: {wrap};
+	"
+>
 	<slot />
 </div>
-
-<style>
-	div {
-		display: flex;
-	}
-</style>
