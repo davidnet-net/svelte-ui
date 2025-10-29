4d3cd7fe61183e07659c570833fd8c2f07ec33f4
dedestem <david@davidnet.net>
2025-10-29 14:34:55 +0100

    Export the stuff

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 export * from "$lib/components/index.js";
 export * from "$lib/stores/index.js";
 export * from "$lib/metadata.js";
+export * from "$lib/utils/index.js";
+export * from "$lib/session.js";
