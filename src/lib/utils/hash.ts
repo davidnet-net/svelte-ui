dbd2a8757cfdd21f1c9647160e2329d99e6d8956
dedestem <david@davidnet.net>
2025-12-03 21:46:07 +0100

    FMT

--- src/lib/utils/hash.ts
+++ src/lib/utils/hash.ts
@@ -1,17 +1,17 @@
 import CryptoJS from "crypto-js";
 
 export async function hashSHA256(data: string): Promise<string> {
-  // Check if Web Crypto API is available
-  if (typeof crypto !== "undefined" && crypto.subtle && crypto.subtle.digest) {
-    const encoder = new TextEncoder();
-    const buffer = encoder.encode(data);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", buffer);
+	// Check if Web Crypto API is available
+	if (typeof crypto !== "undefined" && crypto.subtle && crypto.subtle.digest) {
+		const encoder = new TextEncoder();
+		const buffer = encoder.encode(data);
+		const hashBuffer = await crypto.subtle.digest("SHA-256", buffer);
 
-    return Array.from(new Uint8Array(hashBuffer))
-      .map((x) => x.toString(16).padStart(2, "0"))
-      .join("");
-  } else {
-    // Fallback to CryptoJS
-    return CryptoJS.SHA256(data).toString(CryptoJS.enc.Hex);
-  }
+		return Array.from(new Uint8Array(hashBuffer))
+			.map((x) => x.toString(16).padStart(2, "0"))
+			.join("");
+	} else {
+		// Fallback to CryptoJS
+		return CryptoJS.SHA256(data).toString(CryptoJS.enc.Hex);
+	}
 }
