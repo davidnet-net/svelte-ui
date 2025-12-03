69ed1af47d210a62ddb7378268984a9c95c1e68d
dedestem <david@davidnet.net>
2025-12-03 21:09:23 +0100

    FMT

--- src/lib/utils/hash.ts
+++ src/lib/utils/hash.ts
@@ -1,9 +1,9 @@
 async function hashSHA256(data: string) {
-  const encoder = new TextEncoder();
-  const buffer = encoder.encode(data);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', buffer);
+	const encoder = new TextEncoder();
+	const buffer = encoder.encode(data);
+	const hashBuffer = await crypto.subtle.digest("SHA-256", buffer);
 
-  return Array.from(new Uint8Array(hashBuffer))
-    .map(x => x.toString(16).padStart(2, '0'))
-    .join('');
+	return Array.from(new Uint8Array(hashBuffer))
+		.map((x) => x.toString(16).padStart(2, "0"))
+		.join("");
 }
