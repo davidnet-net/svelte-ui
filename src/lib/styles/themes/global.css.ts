b9ef18b5d0bed025bc6854b1f2b89b06cb99a692
dedestem <david@davidnet.net>
2026-01-02 11:38:09 +0100

    Styling: Finish global tokens  - Spacing  - Rounding  - BorderWidth

--- src/lib/styles/themes/global.css.ts
+++ src/lib/styles/themes/global.css.ts
@@ -11,8 +11,32 @@ export const global = createGlobalTheme(':root', {
       regular: '400',
       bold: '700',
     }
+  },
+
+  spacing: {
+    '0': '0',
+    '1': '0.25rem',    // 4px
+    '1.5': '0.375rem', // 6px
+    '2': '0.5rem',     // 8px
+    '2.5': '0.625rem', // 10px
+    '3': '0.75rem',    // 12px
+    '4': '1rem',       // 16px
+    '5': '1.25rem',    // 20px
+    '6': '1.5rem',     // 24px
+  },
+
+  radius: {
+    '0': '0',
+    '1': '2px',
+    '2': '4px',
+    '3': '8px',
+    '4': '12px',
+    '5': '16px',
+    'full': '9999px',
+  },
+
+  borderWidth: {
+    standard: '1px', 
+    thick: '2px',
   }
-  // Todo:
-  // Spacing
-  // Rounding
 });
