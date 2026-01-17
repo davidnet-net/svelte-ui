8ffde43e6838a4a50e1627d234ae7c6c2e060267
dedestem <david@davidnet.net>
2026-01-17 16:50:17 +0100

    CQC: Add playwright and vitest & PKG: Add mdsvex mdsvex is svelte inside markdown

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), mdsvex()],
 
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	extensions: [".svelte", ".svx"]
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { playwright } from "@vitest/browser-playwright";
 import { execSync } from "child_process";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 import pkg from "./package.json" with { type: "json" };
 
 const pkgVersion = pkg.version;
 const rawRepoUrl = typeof pkg.repository === "string" ? pkg.repository : pkg.repository.url;
-
 const repoUrl = rawRepoUrl.replace(/^git\+/, "").replace(/\.git$/, "");
-
 const commitHash = execSync("git rev-parse HEAD").toString().trim();
 const commitUrl = `${repoUrl}/commit/${commitHash}`;
 
@@ -23,9 +22,11 @@ export default defineConfig({
 			strategy: ["localStorage", "preferredLanguage", "baseLocale"],
 			localStorageKey: "language"
 		}),
+
 		sveltekit(),
 		vanillaExtractPlugin()
 	],
+
 	define: {
 		__DDS_INFO__: {
 			version: JSON.stringify(pkgVersion), // Added version
@@ -33,5 +34,39 @@ export default defineConfig({
 			commitUrl: JSON.stringify(commitUrl),
 			buildTime: JSON.stringify(new Date().toISOString())
 		}
+	},
+
+	test: {
+		expect: { requireAssertions: true },
+
+		projects: [
+			{
+				extends: "./vite.config.ts",
+
+				test: {
+					name: "client",
+
+					browser: {
+						enabled: true,
+						provider: playwright(),
+						instances: [{ browser: "chromium", headless: true }]
+					},
+
+					include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+					exclude: ["src/lib/server/**"]
+				}
+			},
+
+			{
+				extends: "./vite.config.ts",
+
+				test: {
+					name: "server",
+					environment: "node",
+					include: ["src/**/*.{test,spec}.{js,ts}"],
+					exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"]
+				}
+			}
+		]
 	}
 });
