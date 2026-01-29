4e1cff533fa12890c5e08079d552a35d9e2d5e5e
dedestem <david@davidnet.net>
2026-01-29 18:55:19 +0100

    Depedency: Remove mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess()],
 
 	kit: { adapter: adapter() },
 	extensions: [".svelte", ".svx"]
