9d521efdee8fdcc2384d2a34cc5d7aad379607b5
dedestem <david@davidnet.net>
2026-04-03 13:27:26 +0200

    Feature: Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { Icon } from "$lib";
+	import { styles } from "./error.css";
+
+	import { page } from "$app/state";
+	import Button from "$lib/components/input/Button/Button.svelte";
+	import { appState } from "$lib/engines/appStateEngine.svelte";
+	import { token } from "$lib/styles/designTokens";
+</script>
+
+<div class={styles.pageContainer}>
+	{#if page.status === 404}
+		<div class={styles.backgroundContainer}>
+			<div class={styles.background}>
+				<div class={styles.iconBackground}>
+					<Icon icon="not_listed_location" size="inherit" color="danger" />
+				</div>
+				{#if !appState.isMobile}
+					<h1 class={styles.errorHeader}>{page.error?.message}</h1>
+				{/if}
+			</div>
+		</div>
+		{#if appState.isMobile}
+			<h1
+				style="transform: translateY(-6vw); padding-bottom: {token.global.spacing.large};"
+				class={styles.errorHeader}>
+				{page.error?.message}
+			</h1>
+		{/if}
+		<div class={styles.buttonContainer}>
+			<Button iconbefore="arrow_back" onclick={() => history.back()}>Back</Button>
+		</div>
+	{:else}
+		<div class={styles.backgroundContainer}>
+			<div class={styles.background}>
+				<div class={styles.iconBackground}>
+					<Icon icon="release_alert" size="inherit" color="danger" />
+				</div>
+				{#if !appState.isMobile}
+					<h1 class={styles.errorHeader}>{page.error?.message}</h1>
+				{/if}
+			</div>
+		</div>
+		{#if appState.isMobile}
+			<h1
+				style="transform: translateY(-6vw); padding-bottom: {token.global.spacing.large};"
+				class={styles.errorHeader}>
+				{page.error?.message}
+			</h1>
+		{/if}
+		<div class={styles.buttonContainer}>
+			<Button iconbefore="arrow_back" onclick={() => history.back()}>Back</Button>
+			<Button onclick={() => window.location.reload()}>Retry</Button>
+		</div>
+	{/if}
+</div>
---
+++ src/routes/error.css.ts
@@ -0,0 +1,66 @@
+import { style } from "@vanilla-extract/css";
+import { token } from "../lib/styles/designTokens.ts";
+
+const pageContainer = style({
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	justifyContent: "center",
+	height: "100%"
+});
+
+const background = style({
+	width: "clamp(16rem, 30dvw, 30rem)",
+	padding: token.global.spacing.large,
+	backgroundColor: token.theme.color.surface.sunken.normal,
+	aspectRatio: "1",
+	clipPath: `shape(
+    from 75% 25%,
+    curve to 75% 75% with 100% 50%,
+    curve to 25% 75% with 50% 100%,
+    curve to 25% 25% with 0% 50%,
+    curve to 75% 25% with 50% 0%
+  )`,
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	justifyContent: "center",
+	transform: "translateY(-5vw)"
+});
+
+const iconBackground = style({
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+	padding: token.global.spacing.large,
+	borderRadius: token.global.radius.full,
+	fontSize: token.global.font.size.giant
+});
+
+const errorHeader = style({
+	fontSize: token.global.font.size.large
+});
+
+const buttonContainer = style({
+	transform: "translateY(-6vw)",
+	display: "flex",
+	gap: token.global.spacing.medium
+});
+
+export const backgroundContainer = style({
+	filter: `
+        drop-shadow(-1px -1px 0 ${token.theme.color.border.default}) 
+        drop-shadow(1px -1px 0 ${token.theme.color.border.default}) 
+        drop-shadow(-1px 1px 0 ${token.theme.color.border.default}) 
+        drop-shadow(1px 1px 0 ${token.theme.color.border.default})
+    `
+});
+
+export const styles = {
+	pageContainer,
+	backgroundContainer,
+	iconBackground,
+	errorHeader,
+	background,
+	buttonContainer
+};
