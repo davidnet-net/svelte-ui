29352009bd14ce356698845f83f559175b64ff31
dedestem <david@davidnet.net>
2026-04-04 12:21:43 +0200

    Feat: Start working on content page

---
+++ src/routes/foundations/content/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Header from "$lib/internal/components/Header/Header.svelte";
+	import NavigationData from "$lib/internal/components/NavigationData.svelte";
+	import * as m from "$lib/paraglide/messages.js";
+
+	import { styles } from "./page.css.ts";
+</script>
+
+<NavigationData />
+
+<Header
+	heading={m.docs_page_foundations_content_header_heading()}
+	paragraph={m.docs_page_foundations_content_header_paragraph()} />
+
+<div class={styles.pageContainer}></div>
