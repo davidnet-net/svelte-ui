38e7952dd80caa39f48cbb8fab1f54d756b6bc33
dedestem <david@davidnet.net>
2026-04-22 19:47:12 +0200

    Feat: Added patchFetch  - Also added basefetch for modulairity

--- src/lib/utils/fetches.ts
+++ src/lib/utils/fetches.ts
@@ -3,18 +3,41 @@ import { appState } from "$lib/engines/appStateEngine.svelte";
 import { generateUUIDv7 } from "./crypto";
 
 // Add parameter sendAuth
-export async function postFetch(url: string, data: Record<string, unknown>) {
-	const correlationID = generateUUIDv7(); // Replace with open telemetry traceID
+
+async function baseFetch(
+	url: string,
+	method: string,
+	data: Record<string, unknown>,
+	customHeaders: Record<string, string> = {}
+) {
+	const correlationID = generateUUIDv7();
 
 	const result = await fetch(url, {
-		method: "POST",
+		method,
 		headers: {
 			"Content-Type": "application/json",
 			"x-Tab-Session-ID": appState.tabID as string,
-			"x-Correlation-ID": correlationID
+			"x-Correlation-ID": correlationID,
+			...customHeaders
 		},
 		body: JSON.stringify(data)
 	});
 
 	return result.json();
+}
+
+export async function postFetch(
+	url: string,
+	data: Record<string, unknown>,
+	headers?: Record<string, string>
+) {
+	return baseFetch(url, "POST", data, headers);
+}
+
+export async function patchFetch(
+	url: string,
+	data: Record<string, unknown>,
+	headers?: Record<string, string>
+) {
+	return baseFetch(url, "PATCH", data, headers);
 }
