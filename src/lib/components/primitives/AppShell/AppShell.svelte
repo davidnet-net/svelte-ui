8ee677034d3bc56224437ab9b10f6174bf5c3f2a
dedestem <david@davidnet.net>
2026-07-21 17:13:59 +0200

    Package: 2.0.37

--- src/lib/components/primitives/AppShell/AppShell.svelte
+++ src/lib/components/primitives/AppShell/AppShell.svelte
@@ -207,7 +207,7 @@
 								<Dropdown bind:isOpen={isAvatarOpened} offset={20}>
 									{#snippet trigger()}
 										<Avatar
-											src=""
+											src={identityState.user!.avatarURL}
 											size="xlarge"
 											alt={library_messages.lib_component_account_menu_alt()}
 											onclick={() => {
